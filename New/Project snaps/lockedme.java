224435a8b4ffd98c0f97005550d53cdf8ac986b9
Robin <robinkm971@gmail.com>
2021-03-07 21:31:15 +0530

    Final corrections for Invalid entry: Robin

--- New/Project snaps/lockedme.java
+++ New/Project snaps/lockedme.java
@@ -14,10 +14,11 @@ public class LockedMe {
 	public static void main(String args[]) throws IOException
 	{
 	
-	System.out.println("****************************************************************");
-	System.out.println("****************************************************************");
-	System.out.println("*********************  LOCKED ME. COM  ********************");
-	System.out.println("****************************************************************");
+	System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+	System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+	System.out.println(":::::::::::::::::::::::::: LOCKED ME.COM  ::::::::::::::::::::::");
+	System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+	System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
 	System.out.println("Developer: Robin K M ");
 	System.out.println();
 	do {
@@ -47,12 +48,17 @@ public class LockedMe {
 			System.out.println("Good bye");
 			System.exit(0);
 		}
+		else if(c!='y' || c !='Y'|| c !='N' || c != 'n') {
+			System.out.println("Oops! you have entered invalid input \nPlease enter a valid option\n\n");
+			   process();
+		}
 	}
 //View files	
 //==================================================================================================
 	public void userInput_Reg() {
 		try {
-		File file = new File("C:\\Users\\Rmath\\Desktop\\New folder");
+		File file = new File("C:\\Users\\Rmath\\Desktop\\New folder");	
+	
 		if(file.isDirectory())
 		{
 			List<String> listFile = Arrays.asList(file.list());
@@ -60,12 +66,14 @@ public class LockedMe {
 				System.out.println(s);
 				System.out.println();
 			}
-			
 		}
-		else{
+	
+			else
+		{
 			System.out.println(file.getAbsolutePath() + " is not a directory");
 		}
-	}
+		}
+	
 catch(ArithmeticException e) {
 	System.out.println("There is an arithmetic exception ");
 }
@@ -83,15 +91,22 @@ catch(ArithmeticException e) {
 		File file = new File("C:\\Users\\Rmath\\Desktop\\New folder");
 
 		LockedMe obj = new LockedMe();
+		try {
 		System.out.println(" 1. View files");
 		System.out.println(" 2. File mainpulation options");
 		System.out.println(" 3. Exit");
 		System.out.println();
 
-		System.out.println("Enter Your Choice: ");
+		System.out.print(" Enter Your Choice: ");
 
 	   num =  input.nextInt();
-		 
+		}
+		catch(Exception d) {
+			if(opt != 1 || opt != 2 || opt != 3) {
+				   System.out.println("Oops! you have entered invalid input \nPlease enter a valid option\n\n");
+				   process();
+		}
+		}
 		   switch(num){  
 //Manipulation options		   
 //==================================================================================================		 
@@ -101,11 +116,9 @@ catch(ArithmeticException e) {
 		           break ;  
 		   case 2:  
 			   try {
-		   System.out.println("Please select from the available options");
-		   System.out.println("Options:\n"); 
-		   
-		   
-		   System.out.println(" 1. Create a new file");
+		    System.out.println("Please select from the available options");
+		    System.out.println("Options:\n"); 	   
+		    System.out.println(" 1. Create a new file");
 			System.out.println(" 2. Delete a file");
 			System.out.println(" 3. Search for a file");
 			System.out.println(" 4. Return to Main context");
@@ -116,7 +129,7 @@ catch(ArithmeticException e) {
 			   catch(Exception e)
 			   {
 		   if(opt != 1 || opt != 2 || opt != 3 || opt != 4 ) {
-			   System.out.println("\nPlease enter valid option\n\n");
+			   System.out.println("Oops! you have entered invalid input \nPlease enter a valid option\n\n");
 			   process();
 			   }
 			   }  
@@ -165,14 +178,16 @@ catch(ArithmeticException e) {
 		   case 4: System.out.println("Welcome back\n");
 			main(null);
 			break;
-			
+		   default: System.out.println("Wrong input!\nPlease select from the available options");
+			 process();
+	}	
 //==================================================================================================
 			
-	}
+	
 		   case 3: System.out.println("Good bye ");
 			System.exit(0);
 			break;
-			 default: System.out.println("Please select from the available options");
+		   default: System.out.println("Wrong input!\nPlease select from the available options");
 			 process();	 		   
 		   }
 	}
@@ -188,7 +203,7 @@ catch(ArithmeticException e) {
 					if(loc[i].getName().contains(h)) {
 							flag=true;
 							if(loc[i].isFile()) {
-								System.out.println("The keyword has retured the below file");
+								System.out.println("The keyword has retured the below files");
 								System.out.println(loc[i].getName());
 												}
 							else if(loc[i].isDirectory()) {
